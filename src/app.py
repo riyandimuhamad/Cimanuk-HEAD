43cd75023d10db8bcee20f31b86aa8a2715ac2f5
riyandimuhamad <riyandiiimuhammad@gmail.com>
2026-08-18 21:37:27 +0700

    style: Merampingkan judul tab browser menjadi lebih natural

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ import geopandas as gpd
 
 # Konfigurasi Halaman (Harus diletakkan paling atas)
 st.set_page_config(
-    page_title="Cimanuk-HEAD | Hydrological DSS",
+    page_title="Dasbor Cimanuk-HEAD",
     layout="wide",
     initial_sidebar_state="expanded"
 )
